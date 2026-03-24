34ba56d596809157611cee429301475ecf103429
manoharan12105-beep <manoharan12105@gmail.com>
2026-03-24 16:08:49 +0530

    Create vehicle.java

---
+++ Long-Programming/Day_03_24_2026_Build_Parking_System_V1/vehicle.java
@@ -0,0 +1,33 @@
+package Day_03_24_2026_Build_Parking_System_V1;
+public class Vehicle{
+
+    private String vehicleNumber;
+    private String vehicleType;
+    private String ownerName;
+
+    public String getVehicleNumber() {
+      return vehicleNumber;
+    }
+
+    public void setVehicleNumber(String vehicleNumber) {
+      this.vehicleNumber = vehicleNumber;
+    }
+
+    public String getVehicleType() {
+      return vehicleType;
+    }
+
+    public void setVehicleType(String vehicleType) {
+      this.vehicleType = vehicleType;
+    }
+
+    public String getOwnerName() {
+      return ownerName;
+    }
+
+    public void setOwnerName(String ownerName) {
+      this.ownerName = ownerName;
+    }
+
+    
+}
