a0b745545a3b7135bb89d72fda1c8c305c271cf2
manoharan12105-beep <manoharan12105@gmail.com>
2026-03-24 21:44:37 +0530

    Create Slots.java

---
+++ Long-Programming/Day 01-Build Parking System V1/Slots.java
@@ -0,0 +1,29 @@
+public class Slot {
+    private int slotNumber;
+    private boolean isOccupied;
+    private Vehicle vehicle;
+
+    public int getSlotNumber() {
+        return slotNumber;
+    }
+
+    public void setSlotNumber(int slotNumber) {
+        this.slotNumber = slotNumber;
+    }
+
+    public boolean isOccupied() {
+        return isOccupied;
+    }
+
+    public void setOccupied(boolean occupied) {
+        isOccupied = occupied;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    public void setVehicle(Vehicle vehicle) {
+        this.vehicle = vehicle;
+    }
+}
